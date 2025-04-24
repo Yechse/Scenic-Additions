4c69f594ce4d35053976818db67518e43c3c21c7
Yechse <umutegeileri15@gmail.com>
2025-04-24 23:42:58 +0300

    first commit

---
+++ src/main/java/net/yechse/scenicadditions/ScenicAdditionsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.yechse.scenicadditions;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ScenicAdditionsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
